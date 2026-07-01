37d11bac96c68fecd9e1a8c02cbaa78d0355571e
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2026-07-01 16:06:28 -0400

    Dirac notation

---
+++ Code/gate.py
@@ -0,0 +1,13 @@
+import cirq 
+import cirq_web
+
+qubit = cirq.NamedQubit.range(2, prefix= 'q')
+circuit = cirq.Circuit()
+circuit.append(cirq.X(qubit[0]))
+# circuit.append(cirq.X(qubit[1]))
+vec = cirq.final_state_vector(circuit, ignore_terminal_measurements= True)
+ket = cirq.dirac_notation(vec)
+blc = cirq_web.BlochSphere(state_vector= vec)
+print(circuit)
+print(vec)
+print(ket)
